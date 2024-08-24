84621bafd92c1e1958f5f00faee1ae2b9692f91d
Felipe Lopes <felipemlopes0009@gmail.com>
2024-08-24 11:23:34 -0400

    Added vite-env.d.ts file with ImportMetaEnv and ImportMeta interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
+if (!CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+  <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+    <App />
+  </ClerkProvider>
   //</React.StrictMode>
 );
