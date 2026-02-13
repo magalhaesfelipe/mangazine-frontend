53d97767720e5a7106137774d0d4cf63d5f0c1d2
magalhaesfelipe <ffelipemagallhaes@gmail.com>
2026-02-12 20:10:24 -0400

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,51 +4,53 @@ import { faUser } from "@fortawesome/free-solid-svg-icons"; // Import the specif
 
 const Header = () => {
   return (
-    <div className="absolute -top-0.5 w-full flex items-center justify-baseline mt-[6%]">
-      <NavLink to="/home" className="no-underline ml-[5%]">
-        <div className="flex flex-col relative bg-yellowgreen h-full">
-          <h1 className="text-[35px] font-['Impact'] text-white font-light no-underline absolute top-[-50px]">
+    <div className="absolute top-0 flex h-20 w-full items-center justify-baseline">
+      <NavLink
+        to="/home"
+        className="absolute top-[15%] ml-[3%] flex items-center rounded-md p-5 pt-0 no-underline transition-colors duration-200 hover:bg-zinc-900"
+      >
+        <div className="flex h-full flex-col align-middle">
+          <h1 className="mr-0.5 pt-5 font-['Impact'] text-[35px] font-light text-white no-underline">
             MANGAZINE
           </h1>
-          <h1 className=" text-white text-2xl mr-2 absolute bottom-[-25px] right-[-195px] font-light">
+          <h1 className="ml-0.5 text-2xl font-light text-white">
             「心」が大事
           </h1>
         </div>
       </NavLink>
       <div className="absolute right-[4%] flex flex-row-reverse items-center">
-        <div className="ml-[20px]"></div>
-
         <NavLink
           to="/auth"
-          className="ml-[30px] no-underline text-white pt-[9px] pb-[7px] pr-[10px] pl-[10px] border-solid border-2 border-transparent hover:border-white transition-all duration-100"
+          className="ml-[30px] border-2 border-solid border-transparent pt-[9px] pr-[10px] pb-[7px] pl-[10px] text-white no-underline transition-all duration-100 hover:border-white"
         >
           <div className="">
             AUTH
-            <FontAwesomeIcon icon={faUser} className="ml-1.5" /> {/* Use the icon */}
+            <FontAwesomeIcon icon={faUser} className="ml-1.5" />{" "}
+            {/* Use the icon */}
           </div>
         </NavLink>
 
         <NavLink
           to="/about"
-          className="ml-[30px] no-underline text-white pt-[9px] pb-[7px] pr-[10px] pl-[10px] border-solid border-2 border-transparent hover:border-white transition-all duration-100"
+          className="ml-[30px] border-2 border-solid border-transparent pt-[9px] pr-[10px] pb-[7px] pl-[10px] text-white no-underline transition-all duration-100 hover:border-white"
         >
           ABOUT
         </NavLink>
         <NavLink
           to="/lists"
-          className="ml-[30px] no-underline text-white pt-[9px] pb-[7px] pr-[10px] pl-[10px] border-solid border-2 border-transparent hover:border-white transition-all duration-100"
+          className="ml-[30px] border-2 border-solid border-transparent pt-[9px] pr-[10px] pb-[7px] pl-[10px] text-white no-underline transition-all duration-100 hover:border-white"
         >
           LISTS
         </NavLink>
         <NavLink
           to="/readlist"
-          className="ml-[30px] no-underline text-white pt-[9px] pb-[7px] pr-[10px] pl-[10px] border-solid border-2 border-transparent hover:border-white transition-all duration-100"
+          className="ml-[30px] border-2 border-solid border-transparent pt-[9px] pr-[10px] pb-[7px] pl-[10px] text-white no-underline transition-all duration-100 hover:border-white"
         >
           READLIST
         </NavLink>
         <NavLink
           to="/home"
-          className="ml-[30px] no-underline text-white pt-[9px] pb-[7px] pr-[10px] pl-[10px] border-solid border-2 border-transparent hover:border-white transition-all duration-100"
+          className="ml-[30px] border-2 border-solid border-transparent pt-[9px] pr-[10px] pb-[7px] pl-[10px] text-white no-underline transition-all duration-100 hover:border-white"
         >
           HOME
         </NavLink>
