5b15f4819a7981d2debbeea2c591b0495e4c4135
felipe.magalhaes <ffelipemagallhaes@gmail.com>
2026-02-17 22:37:21 -0400

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,15 @@
-import classes from "./footer.module.css";
 import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
   const navigate = useNavigate();
-
-  const handleContactClick = () => {
-    navigate("/contact");
-  };
+  const handleContactClick = () => navigate("/contact");
 
   return (
     <div className="mb-5 flex items-center justify-center hover:cursor-pointer">
-      <div className="w-[30%]">
+      <div className="flex w-[90%] justify-center">
         <p
           onClick={handleContactClick}
-          className="w-full cursor-pointer border-2 border-zinc-700 p-5 text-center text-[17px] text-white transition duration-200 hover:border-white"
+          className="w-full cursor-pointer border-b-2 border-zinc-800 p-5 text-center text-[17px] text-white transition duration-200 hover:border-white"
         >
           CONTACT
         </p>
